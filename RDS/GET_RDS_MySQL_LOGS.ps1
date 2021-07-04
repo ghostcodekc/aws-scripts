ebd3bf9b794b6952b2dd5337e47f01e740b6b1a4
ghostcodekc <andrewmgrube@gmail.com>
2021-07-04 09:32:56 -0500

    Added a few powershell scripts from Windows desktop

---
+++ RDS/GET_RDS_MySQL_LOGS.ps1
@@ -0,0 +1,30 @@
+﻿$Region = Read-Host -Prompt 'Input AWS region where the database resides (eg. us-east-1, us-west-2)'
+$dbInstance = Read-Host -Prompt 'Input database instance identifier'
+$LogDay = Read-Host -Prompt 'Input date you would like to retrieve logs from in this format yyyy-MM-dd (ex. 2020-05-01 for May 01, 2020)'
+$slowquerypath = New-Item -ItemType Directory -Force -Path $env:USERPROFILE\Desktop\RDSLogs\$Region\$dbInstance\mysql-slowquery\$LogDay
+$generallogpath = New-Item -ItemType Directory -Force -Path $env:USERPROFILE\Desktop\RDSLogs\$Region\$dbInstance\mysql-general\$LogDay
+
+For ($i=23; $i -gt 0; $i--) {
+    if ($i -ge 10) {
+        Write-Host "Downloading log file: slowquery/mysql-slowquery.log.$LogDay.$i"
+        aws rds download-db-log-file-portion --db-instance-identifier $dbInstance --log-file-name "slowquery/mysql-slowquery.log.$LogDay.$i" --output text > $slowquerypath\mysql-slowquery.log.$LogDay.$i --region $Region
+        Write-Host "Downloading log file: slowquery/mysql-general.log.$LogDay.$i"
+        aws rds download-db-log-file-portion --db-instance-identifier $dbInstance --log-file-name "general/mysql-general.log.$LogDay.$i" --output text > $generallogpath\mysql-general.log.$LogDay.$i --region $Region
+    }
+    elseif ($i -lt 10) {
+        Write-Host "Downloading log file: slowquery/mysql-slowquery.log.$LogDay.0$i"
+        aws rds download-db-log-file-portion --db-instance-identifier $dbInstance --log-file-name "slowquery/mysql-slowquery.log.$LogDay.0$i" --output text > $slowquerypath\mysql-slowquery.log.$LogDay.0$i --region $Region
+        Write-Host "Downloading log file: slowquery/mysql-general.log.$LogDay.0$i"
+        aws rds download-db-log-file-portion --db-instance-identifier $dbInstance --log-file-name "general/mysql-general.log.$LogDay.0$i" --output text > $generallogpath\mysql-general.log.$LogDay.0$i --region $Region
+    }
+    else { 
+        Write-Host "Downloading log file: mysql-slowquery.log"
+        aws rds download-db-log-file-portion --db-instance-identifier $dbInstance --log-file-name "slowquery/mysql-slowquery.log" --output text > $slowquerypath\slowquery.log --region $Region
+        Write-Host "Downloading log file: mysql-general.log"
+        aws rds download-db-log-file-portion --db-instance-identifier $dbInstance --log-file-name "general/mysql-general.log" --output text > $generallogpath\slowquery.log --region $Region
+    }
+
+}
+
+
+Write-Host "Operation Complete. Please go to the following directory to view the log files: $env:USERPROFILE\Desktop\RDSLogs"
